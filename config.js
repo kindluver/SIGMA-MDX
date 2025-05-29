62e5957e35ae0af9716af59ac32ae0931ffe4274
kindluver <happydays3125@Gmail.com>
2025-05-29 23:21:24 +0200

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "",
+SESSION_ID: process.env.SESSION_ID || "SIGMA-MD~exBGTYJS#1kYZKfZfGgvXTbS-R7De1TSl8uQmWVEIQDLW0gak_uA",
 // add your Session Id 
 AUTO_STATUS_SEEN: process.env.AUTO_STATUS_SEEN || "true",
 // make true or false status auto seen
@@ -27,9 +27,9 @@ CUSTOM_REACT_EMOJIS: process.env.CUSTOM_REACT_EMOJIS || "💝,💖,💗,❤️�
 // chose custom react emojis by yourself 
 DELETE_LINKS: process.env.DELETE_LINKS || "false",
 // automatic delete links witho remove member 
-OWNER_NUMBER: process.env.OWNER_NUMBER || "32466304227",
+OWNER_NUMBER: process.env.OWNER_NUMBER || "237692367255",
 // add your bot owner number
-OWNER_NAME: process.env.OWNER_NAME || "ᴍᴜᴢᴀɴ sɪɢᴍᴀ",
+OWNER_NAME: process.env.OWNER_NAME || "DARYL",
 // add bot owner name
 DESCRIPTION: process.env.DESCRIPTION || "ᴘᴏᴡᴇʀᴇᴅ ʙʏ ᴍᴜᴢᴀɴ ꜱɪɢᴍᴀ  ",
 // add bot owner name    
